e6125f4c3cdbc8959ef1b01da42ac73ce852df3c
Balacuit <12411842@benilde.edu.ph>
2026-06-03 20:20:25 +0800

    Add class selection feature

--- generator.py
+++ generator.py
@@ -1,3 +1,9 @@
 def choose_class():
 classes = ["Warrior", "Mage", "Rogue"]
 print("Available Classes:", ", ".join(classes))
+
+def main():
+print("--- Welcome to the Character Generator ---")
+name = input("Enter character name: ")
+char_class = choose_class() # Added by Person B
+print(f"\nCharacter {name} the {char_class} has been created!")
