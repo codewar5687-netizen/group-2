dee4704f9c830e17087577e9bf9393fca58b05e5
Llavore <12312965@benilde.edu.ph>
2026-06-03 20:13:43 +0800

    Added starting gear function

--- generator.py
+++ generator.py
@@ -1,3 +1,16 @@
+# RPG Character Generator
+def main():
+print("--- Welcome to the Character Generator ---")
+# Components will be combined here
+name = "Hero"
+print(f"\nCharacter {name} has been successfully created!")
+if __name__ == "__main__":
+main()
+
+
+
+
+
 def get_starting_gear(char_class):
     gear = {
         "Warrior": ["Iron Sword", "Shield"],
