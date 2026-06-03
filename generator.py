64df65afc1f3d0e1b6f6f6cc50e7b18dcdf0a8fb
Llavore <12312965@benilde.edu.ph>
2026-06-03 20:20:44 +0800

    fixed main function and if statement calling main function

--- generator.py
+++ generator.py
@@ -3,12 +3,13 @@ import random
 
 # RPG Character Generator
 def main():
-print("--- Welcome to the Character Generator ---")
-# Components will be combined here
-name = "Hero"
-print(f"\nCharacter {name} has been successfully created!")
+    print("--- Welcome to the Character Generator ---")
+    # Components will be combined here
+    name = "Hero"
+    print(f"\nCharacter {name} has been successfully created!")
+
 if __name__ == "__main__":
-main()
+    main()
 
 def roll_attributes():
     return {
